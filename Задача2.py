ca4ec806046af75c57f1a77769b42d3fd440588e
AlexBelousoff <108760385+AlexBelousoff@users.noreply.github.com>
2022-07-14 12:31:07 +0300

    Add files via upload

---
+++ Задача2.py
@@ -0,0 +1,11 @@
+age = str('How old are you?')
+# print(type(age))
+
+age_int = int(21)
+# if-elif-else
+if age_int == 21:
+    print('You should visit Holland.')
+elif age_int > 21:
+    print('You should visit Vietnam.')
+else:
+    print('Travel everywhere')
